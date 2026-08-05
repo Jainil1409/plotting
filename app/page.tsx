4a0c42d6214e00b3d4f0282986fceb7b218ceaa5
Jainil1409 <jainilvyas1414@gmail.com>
2026-08-05 12:04:15 +0530

    commented code of exteranlrenderer and objectsymbol3d

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,19 @@
 "use client";
 
 import dynamic from "next/dynamic";
-import { useState } from "react";
-import Navbar, { type Tab } from "@/components/Navbar";
+//import { useState } from "react";
+//import Navbar, { type Tab } from "@/components/Navbar";
 
-const ArcGISMap = dynamic(() => import("@/components/map/ArcGISMap"), { ssr: false });
-const ObjectSymbol3DMap = dynamic(() => import("@/components/map/ObjectSymbol3DMap"), { ssr: false });
+//const ArcGISMap = dynamic(() => import("@/components/map/ArcGISMap"), { ssr: false });
+//const ObjectSymbol3DMap = dynamic(() => import("@/components/map/ObjectSymbol3DMap"), { ssr: false });
 const GoogleMap3D = dynamic(() => import("@/components/map/GoogleMap3D"), { ssr: false });
 
 export default function Home() {
-  const [activeTab, setActiveTab] = useState<Tab>("external");
+  //const [activeTab, setActiveTab] = useState<Tab>("external");
 
   return (
     <div style={{ width: "100vw", height: "100vh", display: "flex", flexDirection: "column" }}>
-      <Navbar active={activeTab} onChange={setActiveTab} />
+      {/* <Navbar active={activeTab} onChange={setActiveTab} />
       <div style={{ flex: 1, marginTop: 52, position: "relative" }}>
         <div style={{ position: "absolute", inset: 0, display: activeTab === "external" ? "block" : "none" }}>
           <ArcGISMap />
@@ -24,7 +24,8 @@ export default function Home() {
         <div style={{ position: "absolute", inset: 0, display: activeTab === "googlemaps" ? "block" : "none" }}>
           <GoogleMap3D />
         </div>
-      </div>
+      </div> */}
+      <GoogleMap3D />
     </div>
   );
 }
